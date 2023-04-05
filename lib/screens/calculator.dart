e7b62c8dfb05cb513e019334be8f693cc71be8eb
Lucas Reis <lucasreis_9@hotmail.com>
2023-04-05 18:08:17 -0300

    Implementando a logica

--- lib/screens/calculator.dart
+++ lib/screens/calculator.dart
@@ -2,6 +2,7 @@ import 'package:calculadora__flutter/components/display.dart';
 import 'package:calculadora__flutter/components/keyboard.dart';
 import 'package:calculadora__flutter/models/memory.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 class Calculator extends StatefulWidget {
   const Calculator({super.key});
@@ -21,6 +22,11 @@ class _CalculatorState extends State<Calculator> {
 
   @override
   Widget build(BuildContext context) {
+    // Define a orientação do celular
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.portraitUp,
+    ]);
+
     return MaterialApp(
       home: Column(
         children: [
